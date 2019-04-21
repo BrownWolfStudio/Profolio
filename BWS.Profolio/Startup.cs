b1b534a19b0d8ecb7aaf454f1799901ca674a684
TheBinaryGuy <heet_mehta@hotmail.com>
2019-04-21 20:05:12 +0530

    Removed comments.

--- BWS.Profolio/Startup.cs
+++ BWS.Profolio/Startup.cs
@@ -50,29 +50,6 @@ namespace BWS.Profolio
                 options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
                 options.TokenEndpoint = "https://github.com/login/oauth/access_token";
                 options.UserInformationEndpoint = "https://api.github.com/user";
-
-                //options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-                //options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-                //options.ClaimActions.MapJsonKey("urn:github:login", "login");
-                //options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
-                //options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
-
-                //options.Events = new OAuthEvents
-                //{
-                //    OnCreatingTicket = async context =>
-                //    {
-                //        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-                //        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                //        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-                //        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-                //        response.EnsureSuccessStatusCode();
-
-                //        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-                //        context.RunClaimActions(user);
-                //    }
-                //};
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
